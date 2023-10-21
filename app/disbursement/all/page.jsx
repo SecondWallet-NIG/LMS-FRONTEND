861345cba82cfefd9d6aeceb67744727f75f53b3
Babajide Ojo <babajide.ojo@konga.com>
2023-10-21 06:22:42 +0100

    chore

--- app/disbursement/all/page.jsx
+++ app/disbursement/all/page.jsx
@@ -20,7 +20,7 @@ const AllDisburments = () => {
         </div>
         <ReusableDataTable
           onClickRow={"/customers/profile"}
-          apiEndpoint="http://localhost:8000/api/customer/profile-information"
+          apiEndpoint="https://secondwallet-stag.onrender.com/api/customer/profile-information"
           initialData={[]}
           headers={headers}
         />
