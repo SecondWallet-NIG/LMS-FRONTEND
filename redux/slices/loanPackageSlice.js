94548aa0c065e92a800e242764b2d0334dafddcb
codeAmbivert <titilopechisom20@gmail.com>
2023-11-20 12:32:33 +0100

    loan and packages

--- redux/slices/loanPackageSlice.js
+++ redux/slices/loanPackageSlice.js
@@ -1,68 +1,73 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { API_URL } from '@/constant';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { API_URL } from "@/constant";
 
 let user;
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   user = JSON.parse(localStorage.getItem("user"));
 }
 
-export const createLoanPackage = createAsyncThunk('loanPackage/create', async (payload) => {
+export const createLoanPackage = createAsyncThunk(
+  "loanPackage/create",
+  async (payload) => {
     try {
-      const response = await axios.post(API_URL +'/loan-package/create', {
-        headers: {
-          Authorization: `Bearer ${user?.data?.token}`
-        }
-      }, payload);
+      const response = await axios.post(
+        API_URL + "/loan-package/create",
+        {
+          headers: {
+            Authorization: `Bearer ${user?.data?.token}`,
+          },
+        },
+        payload
+      );
       return response.data;
     } catch (error) {
       if (error.response.data.error) {
-        throw new Error(error.response.data.error)
+        throw new Error(error.response.data.error);
       }
-   
     }
-  });
+  }
+);
 
-export const getLoanPackage = createAsyncThunk('LoanPackage/all', async () => {
+export const getLoanPackage = createAsyncThunk("LoanPackage/all", async () => {
   const response = await axios.get(`${API_URL}/loan-package/all`, {
     headers: {
-      Authorization: `Bearer ${user?.data?.token}`
-    }
+      Authorization: `Bearer ${user?.data?.token}`,
+    },
   });
   return response.data;
 });
 
-
 const LoanPackageSlice = createSlice({
-  name: 'LoanPackage',
+  name: "LoanPackage",
   initialState: {
     data: null,
-    loading: 'idle',
+    loading: "idle",
     error: null,
   },
   reducers: {
     // Reducer logic for actions not handled by createAsyncThunk
     clearUserState: (state) => {
       state.data = null;
-      state.loading = 'idle';
+      state.loading = "idle";
       state.error = null;
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(getLoanPackage.pending, (state) => {
-        state.loading = 'pending';
+        state.loading = "pending";
         state.error = null;
       })
       .addCase(getLoanPackage.fulfilled, (state, action) => {
-        state.loading = 'succeeded';
+        state.loading = "succeeded";
         state.data = action.payload;
       })
-      .addCase(getLoanPackage.rejected, (state, action,) => {
+      .addCase(getLoanPackage.rejected, (state, action) => {
         console.log("action.error.message", action.error.message);
-        state.loading = 'failed';
+        state.loading = "failed";
         state.error = action.error.message;
-      })
+      });
   },
 });
 
