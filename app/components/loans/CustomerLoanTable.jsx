e9337f84701c11d01d79b05dfd2c5782c09d1e14
codeAmbivert <titilopechisom20@gmail.com>
2024-01-05 14:33:10 +0100

    borrower profile loan/ borrower name and id removal

--- app/components/loans/CustomerLoanTable.jsx
+++ app/components/loans/CustomerLoanTable.jsx
@@ -1,32 +1,29 @@
 "use client";
 import { formatDate } from "@/helpers";
 import ReusableDataTable from "../shared/tables/ReusableDataTable";
-const CustomerLoanTable = ({id}) => {
+const CustomerLoanTable = ({ id }) => {
   const headers = [
- 
     { id: "createdAt", label: "Date Created" },
-    { id: "name", label: "Name & ID" },
-    { id: "loanPackageId", label: "Loan ID & package"},
-    { id: "loanAmount", label: "Loan Amount"},
+    // { id: "name", label: "Name & ID" },
+    { id: "loanPackageId", label: "Loan package & ID" },
+    { id: "loanAmount", label: "Loan Amount" },
     { id: "status", label: "Loan Status" },
   ];
-
 
   const customDataTransformer = (apiData) => {
     return apiData?.map((item) => ({
       id: item._id,
       createdAt: (
         <div className="text-md font-[500] text-gray-700">
-     {formatDate(item.createdAt?.slice(0, 10))}
+          {formatDate(item.createdAt?.slice(0, 10))}
         </div>
       ),
-      name: (
-        <div>
-          <div className="text-md font-[500] text-gray-700">{`${item?.customerId?.firstName} ${item?.customerId?.lastName}`}</div>
-          <div className="text-xs text-gray-500">Borrower&apos;s ID</div>
-
-        </div>
-      ),
+      // name: (
+      //   <div>
+      //     <div className="text-md font-[500] text-gray-700">{`${item?.customerId?.firstName} ${item?.customerId?.lastName}`}</div>
+      //     <div className="text-xs text-gray-500">Borrower&apos;s ID</div>
+      //   </div>
+      // ),
       loanPackageId: (
         <div>
           <div className="text-md font-[500] text-gray-700">{`${item?.loanPackage?.name}`}</div>
@@ -35,50 +32,50 @@ const CustomerLoanTable = ({id}) => {
       ),
       loanAmount: (
         <div>
-          <div className="text-md font-[500] text-gray-700">₦ {item?.loanAmount.toLocaleString()}</div>
+          <div className="text-md font-[500] text-gray-700">
+            ₦ {item?.loanAmount.toLocaleString()}
+          </div>
         </div>
       ),
       status: (
         <button
-        className={`${
-          item.status === "Pending"
-            ? "bg-swIndicatorLightRed"
-            : item.status === "In Progress"
-            ? "bg-swIndicatorYellow"
-            : item.status === "Ready for Disbursal"
-            ? "bg-swIndicatorPurple"
-            : item.status === "Disbursed"
-            ? "bg-swBlue"
-            : item.status === "Fully Paid"
-            ? "bg-swGreen"
-            : "bg-swIndicatorDarkRed"
-        } px-2 py-1 rounded-full text-xs font-normal text-white`}
-      >
-        {item.status}
-      </button>
-      ),  
+          className={`${
+            item.status === "Pending"
+              ? "bg-swIndicatorLightRed"
+              : item.status === "In Progress"
+              ? "bg-swIndicatorYellow"
+              : item.status === "Ready for Disbursal"
+              ? "bg-swIndicatorPurple"
+              : item.status === "Disbursed"
+              ? "bg-swBlue"
+              : item.status === "Fully Paid"
+              ? "bg-swGreen"
+              : "bg-swIndicatorDarkRed"
+          } px-2 py-1 rounded-full text-xs font-normal text-white`}
+        >
+          {item.status}
+        </button>
+      ),
     }));
   };
   return (
- 
-      <ReusableDataTable
-        dataTransformer={customDataTransformer}
-        onClickRow="/loan-applications/view-loan"
-        headers={headers}
-        initialData={[]}
-        apiEndpoint={`https://secondwallet-stag.onrender.com/api/loan-application/customer/${id}`}
-        // btnText={
-        //   <div className="flex gap-1 items-center p-1">
-        //     <p className="hidden lg:block">create customer</p>
-        //   </div>
-        // }
-        // btnTextClick={() => {
-        //   router.push("/create-borrower");
-        // }}
-        filters={true}
-        pagination={false}
-      />
- 
+    <ReusableDataTable
+      dataTransformer={customDataTransformer}
+      onClickRow="/loan-applications/view-loan"
+      headers={headers}
+      initialData={[]}
+      apiEndpoint={`https://secondwallet-stag.onrender.com/api/loan-application/customer/${id}`}
+      // btnText={
+      //   <div className="flex gap-1 items-center p-1">
+      //     <p className="hidden lg:block">create customer</p>
+      //   </div>
+      // }
+      // btnTextClick={() => {
+      //   router.push("/create-borrower");
+      // }}
+      filters={true}
+      pagination={false}
+    />
   );
 };
 
