851140a2749381d522416702d681bea0473db1eb
codeAmbivert <titilopechisom20@gmail.com>
2024-01-15 06:31:25 +0100

    function check user login on index page load

--- app/page.js
+++ app/page.js
@@ -1,7 +1,23 @@
-"use client"
+"use client";
 
-import LoginScreen from './components/login/LoginScreen';
+import { useEffect, useState } from "react";
+import LoginScreen from "./components/login/LoginScreen";
+import DashboardPage from "./dashboard/page";
 
 export default function Home() {
-  return (<LoginScreen />)
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const _user = JSON.parse(localStorage.getItem("user"));
+    if (_user) {
+      setUser(_user);
+    }
+    setLoading(false);
+    console.log(_user?.data?.success);
+  }, []);
+
+  if (!user?.data && !loading) {
+    return <LoginScreen />;
+  }
+  return <DashboardPage />;
 }
