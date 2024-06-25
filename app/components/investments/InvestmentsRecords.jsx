096abce73cca933d6a3a1882b5931826a81812b6
Godwin <nwfrgodwin@gmail.com>
2024-06-25 18:12:50 +0100

    feat:Investors navigation

---
+++ app/components/investments/InvestmentsRecords.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React, { useState, useEffect } from "react"
+
+export default function InvestmentsRecords () {
+    return (
+        <div>
+            <h1>Investments Records</h1>
+        </div>
+    )
+}
---
+++ app/components/investments/InvestorsRecords.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React, { useState, useEffect } from "react"
+
+export default function InvestorsRecords () {
+    return (
+        <div>
+            <h1>Investors Records</h1>
+        </div>
+    )
+}
