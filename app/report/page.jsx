f1e788a4c6370c8ad5abb070ad19900ed6d5f72d
Godwin Nwafor <nwfrgodwin@gmail.com>
2024-07-05 11:39:09 +0100

    Feat:Expense report section

--- app/report/page.jsx
+++ app/report/page.jsx
@@ -5,6 +5,7 @@ import LoanReports from "../components/report/Loan_Reports/Loan_Reports";
 import SummaryReport from "../components/report/Summary_Report/SummaryReport";
 import { Poppins } from "next/font/google";
 import ManagementReport from "../components/report/Management_Report/MangementReport";
+import ExpenseReport from "../components/report/Expense_Report/ExpenseReport";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -14,10 +15,18 @@ const poppins = Poppins({
 const Report = () => {
   const [reportToggle, setReportToggle] = useState("loan");
   const [searchBtn, setSearchBtn] = useState(false);
+  const selected = 'text-swBlue border-b-swBlue font-semibold'
+  const navs = [
+    { name: 'Loan Reports', selOption: 'loan' },
+    { name: 'Summary Reports', selOption: 'summary' },
+    { name: 'Management Reports', selOption: 'management' },
+    { name: 'Expense Reports', selOption: 'expense' }
+  ]
 
   const handleReports = (id) => {
     setReportToggle(id);
   };
+
   return (
     <DashboardLayout>
       <main className="">
@@ -26,42 +35,21 @@ const Report = () => {
           className={`text-xs sm:text-base flex justify-between items-center font-medium ${poppins.className}`}
         >
           <div className="flex px-5">
-            <div
-              className={`cursor-pointer py-1 px-4 border-b-2 border-transparent ${
-                reportToggle === "loan"
-                  ? "text-swBlue border-b-swBlue font-semibold"
-                  : "text-swGray"
-              }`}
-              onClick={() => {
-                handleReports("loan");
-              }}
-            >
-              Loan Reports
-            </div>
-            <div
-              className={`cursor-pointer py-1 px-4 border-b-2 border-transparent ${
-                reportToggle === "summary"
-                  ? "text-swBlue border-b-swBlue font-semibold"
-                  : "text-swGray"
-              }`}
-              onClick={() => {
-                handleReports("summary");
-              }}
-            >
-              Summary Reports
-            </div>
-            <div
-              className={`cursor-pointer py-1 px-4 border-b-2 border-transparent ${
-                reportToggle === "management"
-                  ? "text-swBlue border-b-swBlue font-semibold"
-                  : "text-swGray"
-              }`}
-              onClick={() => {
-                handleReports("management");
-              }}
-            >
-              Management Reports
-            </div>
+            {navs.map(nav => {
+              return (
+                <div key={nav.selOption}
+                  className={`cursor-pointer py-1 px-4 border-b-2 border-transparent ${reportToggle === nav.selOption
+                    ? selected
+                    : "text-swGray"
+                    }`}
+                  onClick={() => {
+                    handleReports(nav.selOption);
+                  }}
+                >
+                  {nav.name}
+                </div>
+              )
+            })}
           </div>
         </section>
 
@@ -69,6 +57,7 @@ const Report = () => {
           {reportToggle === "loan" && <LoanReports />}
           {reportToggle === "summary" && <SummaryReport />}
           {reportToggle === "management" && <ManagementReport />}
+          {reportToggle === "expense" && <ExpenseReport />}
         </section>
       </main>
     </DashboardLayout>
