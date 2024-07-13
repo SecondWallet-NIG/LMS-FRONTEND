2ce4e48dc3f5588090748e8eb9690bd95773fe8d
Godwin Nwafor <nwfrgodwin@gmail.com>
2024-07-13 08:20:02 +0100

    Feat:Investors Profile

---
+++ app/components/investor-profile/InvestorBioData.jsx
@@ -0,0 +1,98 @@
+"use client"
+import React, { useState, useEffect } from "react"
+import { format } from "date-fns";
+import { bankArr } from "@/constant";
+import Link from "next/link";
+import { FiSmile, FiHash, FiPhone, FiMail } from "react-icons/fi";
+import { LuCalendar } from "react-icons/lu";
+
+
+export default function InvestorBioData({
+    handleInfoHoverIn, data, labelClass, editButton,
+    valueClass, dataClass, detailsHeader
+}) {
+    const pageDetailsData = [
+        { label: <><LuCalendar size={16} />DOB</>, value: <>{data?.data?.dateOfBirth && format(new Date(data?.data?.dateOfBirth), "PPP")}</> },
+        { label: <><FiSmile size={16} /> Gender</>, value: data?.data?.gender },
+        { label: <><FiHash size={16} /> NIN</>, value: data?.data?.nin },
+        { label: <><FiPhone size={16} /> Phone</>, value: data?.data?.phoneNumber },
+        { label: <><FiMail size={16} /> Email</>, value: data?.data?.email }
+    ]
+    const pageAddressData = [
+        { label: 'Country', value: 'Nigeria' },
+        { label: 'State', value: data?.data?.state },
+        { label: 'LGA', value: data?.data?.lga },
+        { label: 'House NO', value: data?.data?.address?.houseNumber },
+        { label: 'Address', value: data?.data?.address?.houseLocation },
+    ]
+    const pageBankData = [
+        { label: 'Name', value: bankArr.find(option => option.value === data?.data?.bankAccount?.bankName)?.label },
+        { label: 'Acc Number', value: data?.data?.bankAccount?.accountNumber },
+        { label: 'Beneficiary', value: data?.data?.bankAccount?.accountName },
+        { label: 'BVN', value: data?.data?.bvn },
+    ]
+
+    const renderPageData = (label, value) => {
+        return (
+            <div className={`${dataClass}`}>
+                <p className={`${labelClass}`}>{label}</p>
+                <p className={`${valueClass}`}>
+                    {value}
+                </p>
+            </div>
+        )
+    }
+    return (
+        <div>
+            {/* Details */}
+            <div className="p-2">
+                <div
+                    className="flex justify-between my-2"
+                    onMouseEnter={() => handleInfoHoverIn("bio-data")}
+                    onMouseLeave={() => handleInfoHoverIn("close")}
+                >
+                    <p className={`${detailsHeader}`}>Details</p>
+                    {editButton}
+                </div>
+                <div className="">
+                    {pageDetailsData.map((d, index) => <div key={index}>{renderPageData(d.label, d.value)}</div>)}
+                    <Link
+                        href={'/investors/create-investor'}
+                        className={`py-1 px-2 text-swTextColor mt-4 mb-2 font-semibold rounded-md outline outline-1 
+                        outline-gray-100 flex gap-2 border w-fit cursor-pointer text-sm
+                    `}>
+                        Add details
+                    </Link>
+                </div>
+            </div>
+
+            {/* Address */}
+            <div className="p-2 border-t-2 border-gray-300">
+                <div
+                    className="text-sm font-semibold flex justify-between"
+                    onMouseEnter={() => handleInfoHoverIn("address")}
+                    onMouseLeave={() => handleInfoHoverIn("close")}
+                >
+                    <p className={`${detailsHeader}`}>Address</p>
+                </div>
+                <div className="mt-2">
+                    {pageAddressData.map((d, index) => <div key={index}>{renderPageData(d.label, d.value)}</div>)}
+                </div>
+            </div>
+
+            {/* Bank Details */}
+            <div className="p-2 border-t-2 border-gray-300 lg:mb-20">
+                <div
+                    className="text-sm font-semibold flex justify-between"
+                    onMouseEnter={() => handleInfoHoverIn("bank")}
+                    onMouseLeave={() => handleInfoHoverIn("close")}
+                >
+                    <p className={`${detailsHeader}`}>Bank Details</p>
+                </div>
+                <div className="">
+                    {pageBankData.map((d, index) => <div key={index}>{renderPageData(d.label, d.value)}</div>)}
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/components/investor-profile/InvestorProfileDocs.jsx
+++ app/components/investor-profile/InvestorProfileDocs.jsx
@@ -8,113 +8,59 @@ import { handleFileExtention } from "../helpers/utils";
 import dynamic from "next/dynamic";
 import { IoMdClose } from "react-icons/io";
 import UploadInvestorProfileDocs from "./UploadInvestorProfileDocs";
+import Button from "../shared/buttonComponent/Button";
 
 const Viewer = dynamic(() => import("react-viewer"), { ssr: false });
 
-const InvestorProfileDocs = (data) => {
-  console.log({ data });
-
+const InvestorProfileDocs = ({
+  data, openModal, labelClass,
+  handleInfoHoverIn, detailsHeader, editButton
+}) => {
   const [url, setUrl] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [uploadModalOpen, setUploadModalOpen] = useState(false);
   const [fieldType, setFieldType] = useState("");
   const [openFileModal, setOpenFileModal] = useState(false);
+  const docButton = `py-1 px-2 text-swTextColor mt-2 mb-4 rounded-md outline outline-1 
+  outline-gray-100 flex gap-2 border w-fit cursor-pointer text-sm`
 
   const handleSetUrl = (content) => {
     setUrl(content);
     setIsOpen(true);
   };
-  console.log("data2", data?.data?.data?.ninDoc);
   return (
-    <main>
-      <div className="flex justify-between pl-4">
-        <div className="pt-4 font-semibold text-xs text-swGray">
-          NIN Document
-        </div>
-        {/* {data?.data?.identityVerification?.applicationForm} */}
-        <div>
-          {data?.data?.data?.ninDoc != null ? (
-            <div>
-              <button
-                onClick={() => {
-                  handleSetUrl(data?.data?.data?.ninDoc);
-                  setOpenFileModal(true);
-                }}
-                className="text-sm text-swGray underline mt-4"
-              >
-                View Docs
-              </button>
-              {handleFileExtention(url) === "pdf" ? (
-                <div
-                  className={`h-full w-full fixed top-0 left-0 bg-black bg-opacity-25 ${
-                    openFileModal ? "flex" : "hidden"
-                  } justify-center items-center text-white z-[110]`}
-                >
-                  <div className="max-w-3xl w-full h-[70%] m-5 p-5 bg-white">
-                    <div className="flex justify-end">
-                      <IoMdClose
-                        size={20}
-                        className="cursor-pointer text-swBlack"
-                        onClick={() => setOpenFileModal(false)}
-                      />
-                    </div>
-                    <iframe src={url} className="h-full w-full"></iframe>
-                  </div>
-                </div>
-              ) : (
-                <>
-                  {typeof window !== "undefined" ? (
-                    <>
-                      <Viewer
-                        visible={openFileModal}
-                        onClose={() => {
-                          setOpenFileModal(false);
-                        }}
-                        images={[url].map((item) => ({
-                          src: item,
-                          key: item,
-                        }))}
-                      />
-                    </>
-                  ) : null}
-                </>
-              )}
-            </div>
-          ) : (
-            <button
-              onClick={() => {
-                setFieldType("ninDoc");
-                setUploadModalOpen(!uploadModalOpen);
-              }}
-              className="text-sm text-swBlue underline mt-4"
-            >
-              Upload File
-            </button>
-          )}
-        </div>
+    <main className="py-5">
+      <div
+        className="flex justify-between pb-3"
+        onMouseEnter={() => handleInfoHoverIn("bio-data")}
+        onMouseLeave={() => handleInfoHoverIn("close")}
+      >
+        <p className={`${detailsHeader}`}>Investor file upload</p>
+        {editButton}
       </div>
-      <div className="flex justify-between pl-4">
-        <div className="pt-4 font-semibold text-xs text-swGray">
-          TAX Document
+
+      {/* Tax Identification Number (TIN) */}
+      <div className="">
+        <div className={`${labelClass}`}>
+          Tax Identification Number (TIN)
         </div>
         {/* {data?.data?.identityVerification?.applicationForm} */}
         <div>
-          {data?.data?.data?.taxDoc != null ? (
+          {data?.data?.taxDoc != null ? (
             <div>
               <button
                 onClick={() => {
-                  handleSetUrl(data?.data?.data?.taxDoc);
+                  handleSetUrl(data?.data?.taxDoc);
                   setOpenFileModal(true);
                 }}
-                className="text-sm text-swGray underline mt-4"
+                className={`${docButton}`}
               >
                 View Docs
               </button>
               {handleFileExtention(url) === "pdf" ? (
                 <div
-                  className={`h-full w-full fixed top-0 left-0 bg-black bg-opacity-25 ${
-                    openFileModal ? "flex" : "hidden"
-                  } justify-center items-center text-white z-[110]`}
+                  className={`h-full w-full fixed top-0 left-0 bg-black bg-opacity-25 ${openFileModal ? "flex" : "hidden"
+                    } justify-center items-center text-white z-[110]`}
                 >
                   <div className="max-w-3xl w-full h-[70%] m-5 p-5 bg-white">
                     <div className="flex justify-end">
@@ -152,34 +98,35 @@ const InvestorProfileDocs = (data) => {
                 setFieldType("taxDoc");
                 setUploadModalOpen(!uploadModalOpen);
               }}
-              className="text-sm text-swBlue underline mt-4"
+              className={`${docButton}`}
             >
               Upload File
             </button>
           )}
         </div>
       </div>
-      <div className="flex justify-between pl-4">
-        <div className="pt-4 font-semibold text-xs text-swGray">
-          BVN Document
+
+      {/* Bank Verification Number (BVN) */}
+      <div className="">
+        <div className={`${labelClass}`}>
+          Bank Verification Number (BVN)
         </div>
         <div>
-          {data?.data?.data?.bvnDoc != null ? (
+          {data?.data?.bvnDoc != null ? (
             <div>
               <button
                 onClick={() => {
-                  handleSetUrl(data?.data?.data?.bvnDoc);
+                  handleSetUrl(data?.data?.bvnDoc);
                   setOpenFileModal(true);
                 }}
-                className="text-sm text-swGray underline mt-4"
+                className={`${docButton}`}
               >
                 View Docs
               </button>
               {handleFileExtention(url) === "pdf" ? (
                 <div
-                  className={`h-full w-full fixed top-0 left-0 bg-black bg-opacity-25 ${
-                    openFileModal ? "flex" : "hidden"
-                  } justify-center items-center text-white z-[110]`}
+                  className={`h-full w-full fixed top-0 left-0 bg-black bg-opacity-25 ${openFileModal ? "flex" : "hidden"
+                    } justify-center items-center text-white z-[110]`}
                 >
                   <div className="max-w-3xl w-full h-[70%] m-5 p-5 bg-white">
                     <div className="flex justify-end">
@@ -217,13 +164,81 @@ const InvestorProfileDocs = (data) => {
                 setFieldType("bvnDoc");
                 setUploadModalOpen(!uploadModalOpen);
               }}
-              className="text-sm text-swBlue underline mt-4"
+              className={`${docButton}`}
             >
               Upload File
             </button>
           )}
         </div>
       </div>
+
+      {/* National Identity Number (NIN) */}
+      <div className="">
+        <div className={`${labelClass}`}>
+          National Identity Number (NIN)
+        </div>
+        {/* {data?.data?.identityVerification?.applicationForm} */}
+        <div>
+          {data?.data?.ninDoc != null ? (
+            <div>
+              <button
+                onClick={() => {
+                  handleSetUrl(data?.data?.ninDoc);
+                  setOpenFileModal(true);
+                }}
+                className={`${docButton}`}
+              >
+                View Docs
+              </button>
+              {handleFileExtention(url) === "pdf" ? (
+                <div
+                  className={`h-full w-full fixed top-0 left-0 bg-black bg-opacity-25 ${openFileModal ? "flex" : "hidden"
+                    } justify-center items-center text-white z-[110]`}
+                >
+                  <div className="max-w-3xl w-full h-[70%] m-5 p-5 bg-white">
+                    <div className="flex justify-end">
+                      <IoMdClose
+                        size={20}
+                        className="cursor-pointer text-swBlack"
+                        onClick={() => setOpenFileModal(false)}
+                      />
+                    </div>
+                    <iframe src={url} className="h-full w-full"></iframe>
+                  </div>
+                </div>
+              ) : (
+                <>
+                  {typeof window !== "undefined" ? (
+                    <>
+                      <Viewer
+                        visible={openFileModal}
+                        onClose={() => {
+                          setOpenFileModal(false);
+                        }}
+                        images={[url].map((item) => ({
+                          src: item,
+                          key: item,
+                        }))}
+                      />
+                    </>
+                  ) : null}
+                </>
+              )}
+            </div>
+          ) : (
+            <button
+              onClick={() => {
+                setFieldType("ninDoc");
+                setUploadModalOpen(!uploadModalOpen);
+              }}
+              className={`${docButton}`}
+            >
+              Upload File
+            </button>
+          )}
+        </div>
+      </div>
+
 
       <CenterModal
         width={"35%"}
