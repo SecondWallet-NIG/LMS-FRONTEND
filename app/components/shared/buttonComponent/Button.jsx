405012e4402879a37f71894423d52dec87888fa3
codeAmbivert <titilopechisom20@gmail.com>
2024-07-18 19:13:55 +0100

    top up modal

--- app/components/shared/buttonComponent/Button.jsx
+++ app/components/shared/buttonComponent/Button.jsx
@@ -1,40 +1,45 @@
 /* eslint-disable react/display-name */
-import React, { forwardRef } from 'react'
-import { cls } from '../../helpers/utils'
+import React, { forwardRef } from "react";
+import { cls } from "../../helpers/utils";
 
 const classes = {
-  base: 'focus:outline-none transition ease-in-out duration-300',
-  disabled: 'opacity-50 cursor-not-allowed',
-  pill: 'rounded',
+  base: "focus:outline-none transition ease-in-out duration-300",
+  disabled: "opacity-50 cursor-not-allowed",
+  pill: "rounded",
   size: {
-    small: 'px-2 py-1 text-sm',
-    normal: 'px-4 py-2',
-    large: 'px-8 py-3 text-lg'
+    small: "px-2 py-1 text-sm",
+    normal: "px-4 py-2",
+    large: "px-8 py-3 text-lg",
   },
   variant: {
-    success:  'outline-none bg-[#E8F7F0] hover:bg-green text-[#107E4B]',
-    primary: 'outline-none bg-swBlue hover:bg-swBlue text-white',
-    secondary: 'outline-none bg-gray-100 hover:bg-swBlue focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 text-gray-900 hover:text-white',
-    danger: 'outline-none bg-red-400 hover:bg-red-500 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 text-white'
-  }
-}
+    success: "outline-none bg-[#E8F7F0] hover:bg-green text-[#107E4B]",
+    primary: "outline-none bg-swBlue hover:bg-swBlue text-white",
+    secondary:
+      "outline-none bg-gray-100 hover:bg-swBlue focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 text-gray-900 hover:text-white",
+    danger:
+      "outline-none bg-red-400 hover:bg-red-500 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 text-white",
+  },
+};
 
 const Button = forwardRef(
   (
     {
       children,
-      type = 'button',
+      type = "button",
       className,
-      variant = 'primary',
-      size = 'normal',
+      onClick,
+      variant = "primary",
+      size = "normal",
       pill,
       disabled = false,
       ...props
-    }, ref
+    },
+    ref
   ) => (
     <button
       ref={ref}
       disabled={disabled}
+      onClick={onClick}
       type={type}
       className={cls(`
                 ${classes.base}
@@ -48,6 +53,7 @@ const Button = forwardRef(
     >
       {children}
     </button>
-  ));
+  )
+);
 
 export default Button;
