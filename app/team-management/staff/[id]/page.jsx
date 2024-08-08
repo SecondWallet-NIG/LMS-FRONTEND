77f207d5fc7f946c6bd49cd3abc7dffad26f6f38
codeAmbivert <titilopechisom20@gmail.com>
2024-08-08 16:33:07 +0100

    chore

--- app/team-management/staff/[id]/page.jsx
+++ app/team-management/staff/[id]/page.jsx
@@ -1,21 +1,13 @@
 "use client";
 import DashboardLayout from "@/app/components/dashboardLayout/DashboardLayout";
-import CenterModal from "@/app/components/modals/CenterModal";
-import EditableButton from "@/app/components/shared/editableButtonComponent/EditableButton";
-import InputField from "@/app/components/shared/input/InputField";
-import SelectField from "@/app/components/shared/input/SelectField";
-import TasksCard from "@/app/components/teamManagement/TasksCard";
 import { getRoles } from "@/redux/slices/roleSlice";
-import { getUserById, updateUser } from "@/redux/slices/userSlice";
+import { getUserById } from "@/redux/slices/userSlice";
 import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { AiOutlineClose } from "react-icons/ai";
 import { FiEdit2, FiPhone, FiUser } from "react-icons/fi";
-import { IoMdCheckmark } from "react-icons/io";
 import { MdOutlineEmail } from "react-icons/md";
-import { Rings } from "react-loader-spinner";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -31,11 +23,6 @@ const Viewer = dynamic(
 const StaffPage = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const [pageState, setPageState] = useState("r&p");
-  const [isOpen, setIsOpen] = useState(false);
-  const [fileError, setFileError] = useState("");
-  const { loading, error, data } = useSelector((state) => state?.user);
-  const { data: roleData } = useSelector((state) => state?.role);
   const tasks = useSelector((state) => state.UserTasks);
   const [profileImg, setProfileImg] = useState(null);
   const [openProfilePic, setOpenProfilePic] = useState(false);
@@ -50,93 +37,15 @@ const StaffPage = () => {
     status: "",
     isRoleAdmin: false,
   });
+  const { data, loading } = useSelector((state) => state.user);
 
-  console.log({ data });
-
-  const adminOptions = [
-    { value: "CEO", label: "CEO" },
-    { value: "CFO", label: "CFO" },
-    { value: "CFO", label: "CFO" },
-    { value: "Director", label: "Director" },
-  ];
-
-  const handleInputChange = async (e) => {
-    let { name, value } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
-
-  const handleSelectChange = async (selectedOption, name) => {
-    setFormData({
-      ...formData,
-      [name]: selectedOption.value,
-    });
-  };
-
-  const handleFileInputChange = (e) => {
-    setFileError("");
-    const files = Array.from(e.target.files);
-    if (e.target.id === "profilePicture" && e.target.files.length > 0) {
-      const fileExtension = files[0].name.split(".").pop().toLowerCase();
-
-      const allowedExtensions = ["jpg", "jpeg", "png"];
-      if (!allowedExtensions.includes(fileExtension)) {
-        setFileError(
-          "Invalid file type. Please select an image (.jpg, .jpeg, .png)."
-        );
-        return;
-      }
-      setFormData((prev) => ({ ...prev, [e.target.id]: files[0] }));
-    } else {
-      setSelectedFiles(files);
-    }
-  };
-
-  const modifyObjects = (arr) => {
-    return Array.isArray(arr)
-      ? arr.map((item) => ({
-          label: item.name,
-          value: item._id,
-        }))
-      : [];
-  };
-
-  const userStatus = [
-    { label: "Active", value: "Active" },
-    { label: "Inactive", value: "Inactive" },
-  ];
-
-  const preventMinus = (e) => {
-    if (/[^0-9,]/g.test(e.key)) {
-      e.preventDefault();
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const payload = new FormData();
-    payload.append("firstName", formData.firstName);
-    payload.append("lastName", formData.lastName);
-    payload.append("email", formData.email);
-    payload.append("phoneNumber", formData.phoneNumber);
-    payload.append("role", formData.role);
-    payload.append("profilePicture", formData.profilePicture);
-
-    dispatch(
-      updateUser({ userId: data?.data?.user?._id, updatedData: payload })
-    )
-      .unwrap()
-      .then((res) => {
-        toast.success("Profile updated successfully");
-        getUserById(data?.data?.user?._id);
-        setIsOpen(false);
-        //  window.location.reload();
-      })
-      .catch((error) => {
-        toast.error("An error occured");
-      });
+  const returnCardDetails = (name, value) => {
+    return (
+      <div>
+        <p>{name}</p>
+        <p className="text-xl font-semibold">{value}</p>
+      </div>
+    );
   };
 
   useEffect(() => {
@@ -184,14 +93,14 @@ const StaffPage = () => {
     >
       <ToastContainer />
       <main>
-        <div className="p-5">
-          <div className="bg-swBlue rounded-2xl p-5 sm:p-10">
-            <div className="grid grid-cols-1 md:flex justify-between items-center gap-10">
-              <div className="flex flex-col items-center gap-5">
+        <div className="p-5 sm:p-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg gap-5">
+            <div className="p-5 border-2 shadow-lg rounded-lg">
+              <div className="flex items-start gap-5">
                 {data?.data?.user?.profilePicture &&
                 data?.data?.user?.profilePicture !== "null" &&
                 data?.data?.user?.profilePicture !== "undefined" ? (
-                  <div className="h-[8rem] w-[8rem] border-2 rounded-md relative overflow-hidden">
+                  <div className="h-[6rem] w-[6rem] rounded-lg text-swBlue border-swBlue relative overflow-hidden">
                     <Image
                       src={data?.data?.user?.profilePicture}
                       alt="profile"
@@ -221,44 +130,36 @@ const StaffPage = () => {
                     ) : null}
                   </div>
                 ) : (
-                  <div className="h-[8rem] w-[8rem] roundeed-md border-2 flex justify-center items-center">
-                    <FiUser size={40} color="white" />
+                  <div className="h-[6rem] w-[6rem] rounded-md text-swBlue border-swBlue border-2 flex justify-center items-center">
+                    <FiUser size={40} />
                   </div>
                 )}
-                <div className="text-center  text-white">
+
+                <div>
                   <div className="flex gap-3 items-end">
                     <p className="font-medium text-2xl whitespace-nowrap">
                       {data?.data?.user?.firstName} {data?.data?.user?.lastName}
                     </p>
-                    <p
-                      className={`${
-                        data?.data?.user?.status === "Active"
-                          ? "bg-[#E7F1FE] text-swBlue"
-                          : "bg-[#F8A9A3] text-white"
-                      } text-xs px-2 py-1 rounded-full text-swBlue`}
-                    >
-                      {data?.data?.user?.status}
-                    </p>
                   </div>
                   <p className="font-light">{data?.data?.user?.email}</p>
                   <p className="font-light">SWL-{data?.data?.user?.staffId}</p>
-                  <div className="flex gap-5 items-center justify-center mt-5">
+                  <div className="flex gap-5 items-center mt-2">
                     <Link
                       href={`mailto:${data?.data?.user?.email}`}
                       target="_blank"
-                      className="hover:bg-white hover:text-swBlue p-2 rounded-md cursor-pointer"
+                      className="hover:border-swBlue border-2 border-transparent hover:text-swBlue p-1 rounded-md cursor-pointer"
                     >
                       <MdOutlineEmail size="22" />
                     </Link>
                     <Link
                       href={`tel:${data?.data?.user?.phoneNumber}`}
-                      className="hover:bg-white hover:text-swBlue p-2 rounded-md cursor-pointer"
+                      className="hover:border-swBlue border-2 border-transparent hover:text-swBlue p-1 rounded-md cursor-pointer"
                     >
                       <FiPhone size="20" />
                     </Link>
                     <Link
                       href={`/team-management/staff/update/${data?.data?.user?._id}`}
-                      className="hover:bg-white hover:text-swBlue p-2 rounded-md cursor-pointer"
+                      className="hover:border-swBlue border-2 border-transparent hover:text-swBlue p-1 rounded-md cursor-pointer"
                       // onClick={() => setIsOpen(true)}
                     >
                       <FiEdit2 size="20" />
@@ -266,6 +167,112 @@ const StaffPage = () => {
                   </div>
                 </div>
               </div>
+
+              <div className="grid grid-cols-2 gap-5 mt-10">
+                {returnCardDetails(
+                  "Status",
+                  // <span
+                  //   className={`${
+                  //     data?.data?.user?.status === "Active"
+                  //       ? "bg-[#E7F1FE] text-swBlue"
+                  //       : "bg-[#F8A9A3] text-white"
+                  //   } text-xs px-2 py-1 rounded-full text-swBlue`}
+                  // >
+                  //   {data?.data?.user?.status}
+                  // </span>
+                  <span
+                    className={`${
+                      data?.data?.user?.status === "Active"
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }`}
+                  >
+                    {data?.data?.user?.status}
+                  </span>
+                )}
+                {returnCardDetails("Total Tasks", tasks?.data?.totalCount || 0)}
+                {returnCardDetails(
+                  "Pending Tasks",
+                  tasks?.data?.pendingCount || 0
+                )}
+                {returnCardDetails(
+                  "Completed Tasks",
+                  tasks?.data?.doneCount || 0
+                )}
+              </div>
+            </div>
+            <div className="p-5 border-2 shadow-lg rounded-lg">
+              <p className="text-2xl font-semibold">Leave Details</p>
+              <div className="grid grid-cols-2 gap-5 mt-10">
+                {returnCardDetails(
+                  "Annual Leave",
+                  `${
+                    data?.data?.employeeBenefit?.benefitType?.leaveTypes
+                      ?.annualLeave || 0
+                  } days`
+                )}
+                {returnCardDetails(
+                  "Sick Leave",
+                  `${
+                    data?.data?.employeeBenefit?.benefitType?.leaveTypes
+                      ?.sickLeave || 0
+                  } days`
+                )}
+                {returnCardDetails(
+                  "Maternity Leave",
+                  `${
+                    data?.data?.employeeBenefit?.benefitType?.leaveTypes
+                      ?.maternityLeave || 0
+                  } days`
+                )}
+                {returnCardDetails(
+                  "Paternity Leave",
+                  `${
+                    data?.data?.employeeBenefit?.benefitType?.leaveTypes
+                      ?.paternityLeave || 0
+                  } days`
+                )}
+                {returnCardDetails(
+                  "Unpaid Leave",
+                  `${
+                    data?.data?.employeeBenefit?.benefitType?.leaveTypes
+                      ?.unpaidLeave || 0
+                  } days`
+                )}
+              </div>
+            </div>
+            <div className="p-5 border-2 shadow-lg rounded-lg">
+              <p className="text-2xl font-semibold">Department Information</p>
+              <div className="grid grid-cols-2 gap-5 mt-10">
+                {returnCardDetails(
+                  "Department Name",
+                  data?.data?.user?.role?.department?.departmentName || "None"
+                )}
+                {returnCardDetails(
+                  "Department Code",
+                  data?.data?.user?.role?.department?.departmentCode || "None"
+                )}
+                {returnCardDetails(
+                  "Department Head",
+                  data?.data?.user?.role?.department?.departmentHead || "None"
+                )}
+                {returnCardDetails(
+                  "Role",
+                  data?.data?.user?.role?.name || "None"
+                )}
+                {returnCardDetails(
+                  "Salary",
+                  `₦ ${
+                    data?.data?.employeeBenefit?.salary?.toLocaleString() || 0
+                  }`
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      {/* <div className="bg-swBlue rounded-2xl p-5 sm:p-10">
+            <div className="grid grid-cols-1 md:flex justify-between items-center gap-10">
               <div className="bg-white rounded-md p-5 w-full grid grid-cols-1 lg:grid-cols-2 gap-5">
                 <div className="">
                   <p className="text-xl font-semibold mb-2">Tasks</p>
@@ -345,7 +352,7 @@ const StaffPage = () => {
                   </div>
                 </div>
               </div>
-              {/*<div>
+              <div>
                 <div className="flex w-80 items-center justify-between text-lg">
                   <p>Role:</p>
                   <p className="">{data?.data?.user?.role?.name}</p>
@@ -366,10 +373,10 @@ const StaffPage = () => {
                     ))}
                   </div>
                 </div>
-              </div>*/}
+              </div>
             </div>
 
-            {/* <div className="mt-10 grid grid-cols-3 gap-5">
+            <div className="mt-10 grid grid-cols-3 gap-5">
               <TasksCard
                 taskName={"Total tasks"}
                 taskAmount={tasks?.data?.totalCount}
@@ -382,10 +389,10 @@ const StaffPage = () => {
                 taskName={"Completed tasks"}
                 taskAmount={tasks?.data?.doneCount}
               />
-            </div> */}
+            </div>
           </div>
         </div>
-        {/* <div>
+        <div>
           <div className="border-b p-2 flex">
             <p
               className={`p-2 ${
@@ -409,7 +416,7 @@ const StaffPage = () => {
               <p>{data?.data?.user?.role?.name}</p>
             </div>
           </div>
-        </div> */}
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 p-5">
           <div className="flex flex-col gap-5">
             <p className="text-xl font-semibold mb-2">Department</p>
@@ -438,14 +445,14 @@ const StaffPage = () => {
           </div>
         </div>
       </main>
-      {/* <CenterModal
+      <CenterModal
         // width={"70%"}
         bgColor={"bg-black bg-opacity-5"}
         isOpen={isOpen}
         onClose={() => {
           setIsOpen(!isOpen);
         }}
-      > */}
+      >
       <main
         className={`fixed top-0 left-0 ${
           isOpen ? "flex" : "hidden"
@@ -458,7 +465,7 @@ const StaffPage = () => {
           <div className="rounded-2xl overflow-auto border border-swGray h-[80%] scrollbar-hide">
             <div className="bg-swBlue flex justify-between items-center p-3 text-white">
               <div>
-                {/* <p className="text-base font-semibold">Add a new staff</p> */}
+                <p className="text-base font-semibold">Add a new staff</p>
                 <p className="text-base font-semibold">
                   {data?.data?.user?.firstName} {data?.data?.user?.lastName}
                 </p>
@@ -471,7 +478,7 @@ const StaffPage = () => {
               />
             </div>
             <div className="pt-8 px-5 pb-16 h-[20rem] overflow-y-auto bg-white relative custom-scrollbar">
-              {/* <div className="flex flex-col"> */}
+              <div className="flex flex-col">
               <div className="flex">
                 <p className="font-semibold my-5 w-1/4">Profile picture</p>
                 <div className="flex flex-col">
@@ -636,7 +643,7 @@ const StaffPage = () => {
             </div>
           </div>
         </form>
-      </main>
+      </main> */}
       {/* </CenterModal> */}
     </DashboardLayout>
   );
