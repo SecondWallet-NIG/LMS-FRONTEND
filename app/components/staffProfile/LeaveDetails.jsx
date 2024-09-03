2fde1f48419618e2cb728ee2fc21996e32fbd46d
codeAmbivert <titilopechisom20@gmail.com>
2024-09-03 19:00:42 +0100

    staff employee benefit btn

--- app/components/staffProfile/LeaveDetails.jsx
+++ app/components/staffProfile/LeaveDetails.jsx
@@ -4,9 +4,10 @@ import Button from "../shared/buttonComponent/Button";
 import { AiOutlinePlus } from "react-icons/ai";
 import RequestLeaveModal from "../leaveRequest/RequestLeaveModal";
 import RightModal from "../modals/RightModal";
-
+import { useRouter } from "next/navigation";
 
 export default function StaffLeaveDetails({ data, id, isDashboard }) {
+  const router = useRouter();
   const [openLeaveRequestModal, setOpenLeaveRequestModal] = useState(false);
   let btnLink, btnText;
   const returnCardDetails = (name, value) => {
@@ -26,66 +27,68 @@ export default function StaffLeaveDetails({ data, id, isDashboard }) {
     btnText = "Add Employee Benefit";
   }
 
-  if (isDashboard) {
-    btnLink = `/employee-dashboard/request-leave/${id}`;
-    btnText = "Request Leave";
-  } else {
-    btnLink = `/team-management/operations/employee-benefit/add-new/${id}`;
-    btnText = "Add Employee Benefit";
-  }
-    return (
-        <div>
-            <div className="p-2 border-2 rounded-lg h-full">
-                <div className="flex justify-between">
-                    <p className="text-md font-medium text-swBlue">Leave Details</p>
-                    {(!data?.employeeBenefit || isDashboard) && (
-                        <Button className="bg-white border border-swBlue text-swBlue hover:text-white hover:bg-swDarkBlue text-xs md:p-[0.37rem] rounded-md ml-2 whitespace-nowrap" onClick={() => setOpenLeaveRequestModal(true) }>
-                          
-                                {!isDashboard && <AiOutlinePlus size={15} />}
-                                <p className="block text-swBlue hover:text-white">{btnText}</p>
-                           
-                        </Button>
-                    )}
-                </div>
-                <div className="grid grid-cols-2 gap-5 mt-2">
-                    {returnCardDetails(
-                        "Annual Leave",
-                        `${data?.employeeBenefit?.benefitType?.leaveTypes
-                            ?.annualLeave || 0
-                        } days`
-                    )}
-                    {returnCardDetails(
-                        "Sick Leave",
-                        `${data?.employeeBenefit?.benefitType?.leaveTypes
-                            ?.sickLeave || 0
-                        } days`
-                    )}
-                    {returnCardDetails(
-                        "Maternity Leave",
-                        `${data?.employeeBenefit?.benefitType?.leaveTypes
-                            ?.maternityLeave || 0
-                        } days`
-                    )}
-                    {returnCardDetails(
-                        "Paternity Leave",
-                        `${data?.employeeBenefit?.benefitType?.leaveTypes
-                            ?.paternityLeave || 0
-                        } days`
-                    )}
-                    {returnCardDetails(
-                        "Unpaid Leave",
-                        `${data?.employeeBenefit?.benefitType?.leaveTypes
-                            ?.unpaidLeave || 0
-                        } days`
-                    )}
-                </div>
-            </div>
-            <RightModal isOpen={openLeaveRequestModal} onClose={() => setOpenLeaveRequestModal(false)} className="mt-8">
-                <RequestLeaveModal onClose={setOpenLeaveRequestModal} />
-            </RightModal>
 
+  return (
+    <div>
+      <div className="p-2 border-2 rounded-lg h-full">
+        <div className="flex justify-between">
+          <p className="text-md font-medium text-swBlue">Leave Details</p>
+          {(!data?.employeeBenefit || isDashboard) && (
+            <Button
+              className="border border-swBlue text-swBlue hover:bg-swDarkBlue text-xs md:p-[0.37rem] rounded-md ml-2 whitespace-nowrap flex gap-1"
+              onClick={() =>
+                isDashboard
+                  ? setOpenLeaveRequestModal(true)
+                  : router.push(btnLink)
+              }
+            >
+              {!isDashboard && <AiOutlinePlus size={15} />}
+              <p className="">{btnText}</p>
+            </Button>
+          )}
         </div>
-    
- 
+        <div className="grid grid-cols-2 gap-5 mt-2">
+          {returnCardDetails(
+            "Annual Leave",
+            `${
+              data?.employeeBenefit?.benefitType?.leaveTypes?.annualLeave || 0
+            } days`
+          )}
+          {returnCardDetails(
+            "Sick Leave",
+            `${
+              data?.employeeBenefit?.benefitType?.leaveTypes?.sickLeave || 0
+            } days`
+          )}
+          {returnCardDetails(
+            "Maternity Leave",
+            `${
+              data?.employeeBenefit?.benefitType?.leaveTypes?.maternityLeave ||
+              0
+            } days`
+          )}
+          {returnCardDetails(
+            "Paternity Leave",
+            `${
+              data?.employeeBenefit?.benefitType?.leaveTypes?.paternityLeave ||
+              0
+            } days`
+          )}
+          {returnCardDetails(
+            "Unpaid Leave",
+            `${
+              data?.employeeBenefit?.benefitType?.leaveTypes?.unpaidLeave || 0
+            } days`
+          )}
+        </div>
+      </div>
+      <RightModal
+        isOpen={openLeaveRequestModal}
+        onClose={() => setOpenLeaveRequestModal(false)}
+        className="mt-8"
+      >
+        <RequestLeaveModal onClose={setOpenLeaveRequestModal} />
+      </RightModal>
+    </div>
   );
 }
