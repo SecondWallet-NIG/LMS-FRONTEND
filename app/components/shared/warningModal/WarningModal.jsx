6918fa19226ddb0fab5c18909a7b35e0c031038f
Tomiwa Bada <tomiwabd@gmail.com>
2025-01-20 12:17:40 +0100

    update warning modal color

--- app/components/shared/warningModal/WarningModal.jsx
+++ app/components/shared/warningModal/WarningModal.jsx
@@ -28,7 +28,7 @@ const ConfirmationModal = ({
           </button>
           <button
             onClick={onConfirm}
-            className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            className="px-4 py-2 text-sm font-medium text-white bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
             {confirmText}
           </button>
